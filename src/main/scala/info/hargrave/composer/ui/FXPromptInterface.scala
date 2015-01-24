25ac1e45f4473619efed17cbed656f0c76260422
Roman Hargrave <roman@hargrave.info>
2015-01-24 14:19:50 -0600

    Add javafx implementation of dialog interface

---
+++ src/main/scala/info/hargrave/composer/ui/FXPromptInterface.scala
@@ -0,0 +1,71 @@
+package info.hargrave.composer.ui
+
+import javafx.scene.control.Dialogs.DialogResponse
+import javafx.stage.Stage
+
+import info.hargrave.composer.ui.PromptInterface.{PromptResponse, PromptType}
+import info.hargrave.composer.util.Localization
+import javafx.scene.control.Dialogs
+
+/**
+ * Implements [[PromptInterface]] for JavaFX using [[javafx.scene.control.Dialogs]]
+ * [[javafx.scene.control.Dialogs]] is a provided by a third-party dialog toolkit for JavaFX 2.0 that aims to mimic
+ * the Dialog functionality provided in JavaFX 8.
+ */
+object FXPromptInterface extends Any with Localization {
+
+    implicit def dialogResponse2PromptResponse(response: DialogResponse): PromptResponse = response match {
+        case DialogResponse.OK      => PromptResponse.ACKNOWLEDGE
+        case DialogResponse.YES     => PromptResponse.CONFIRM
+        case DialogResponse.NO      => PromptResponse.DENY
+        case DialogResponse.CANCEL  => PromptResponse.CANCEL
+        case DialogResponse.CLOSED  => PromptResponse.CLOSED
+    }
+
+    implicit def promptResponse2string(response: PromptResponse): String = response match {
+        case PromptResponse.ACKNOWLEDGE     => t"prompt.acknowledge"
+        case PromptResponse.CONFIRM         => t"prompt.confirm"
+        case PromptResponse.DENY            => t"prompt.deny"
+        case PromptResponse.CANCEL          => t"prompt.cancel"
+        case PromptResponse.CLOSED          => t"prompt.closed"
+    }
+
+    implicit def promptType2DialogFunction(promptType: PromptType): ((Stage, String, String, String) => DialogResponse) = promptType match {
+        case PromptType.ERROR   => Dialogs.showErrorDialog
+        case PromptType.INFO    => (owner, message, masthead, title) => {
+            Dialogs.showInformationDialog(owner, message, masthead, title) // Why does this return nothing?
+            DialogResponse.OK
+        }
+        case PromptType.WARNING => Dialogs.showWarningDialog
+    }
+}
+final class FXPromptInterface extends PromptInterface {
+    import info.hargrave.composer.stage
+    import FXPromptInterface._
+
+    /**
+     * Display a confirmation prompt, offering the user two-to-three options (Confirm, Deny, Cancel)
+     *
+     * @param title         prompt title
+     * @param banner        banner text of the prompt (if the implementation supports it). should disambiguate the title.
+     * @param body          body text of the prompt
+     * @param cancelable    whether or not the user should be allowed to exit the dialog without selecting an explicit action
+     * @return user response
+     */
+    override def displayConfirmationPrompt(title: String, banner: String, body: String, cancelable: Boolean): PromptResponse = {
+        Dialogs.showConfirmDialog(stage, body, banner, title)
+    }
+
+    /**
+     * Display a prompt, offering only an action to acknowledge the prompt.
+     *
+     * @param title         prompt title
+     * @param banner        banner text (if available). should disambiguate the title.
+     * @param body          body text of the prompt
+     * @param promptType    [[PromptType prompt type]] to be shown to the user
+     * @return user response
+     */
+    override def displayNotificationPrompt(title: String, banner: String, body: String, promptType: PromptType): PromptResponse = {
+        promptType(stage, body, banner, title)
+    }
+}
