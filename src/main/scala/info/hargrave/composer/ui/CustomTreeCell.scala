fc5ea2178a1dcaf5e02cbacee169e9de32b41b3a
Roman Hargrave <roman@hargrave.info>
2015-01-30 17:58:10 -0600

    inherit SFXDelegate

--- src/main/scala/info/hargrave/composer/ui/CustomTreeCell.scala
+++ src/main/scala/info/hargrave/composer/ui/CustomTreeCell.scala
@@ -4,12 +4,15 @@ import javafx.scene.control.{TreeCell => JFXTreeCell}
 
 import info.hargrave.composer.ui.CustomTreeCell.JFXTreeCellUpdateDelegate
 
+import scalafx.delegate.SFXDelegate
 import scalafx.scene.control.TreeCell
 
 /**
  * Provides a CustomTreeCell that allows for implementation of the UpdateItem method
  */
-abstract class CustomTreeCell[T](override val delegate: JFXTreeCellUpdateDelegate[T] = new JFXTreeCellUpdateDelegate[T]) extends TreeCell[T]  {
+abstract class CustomTreeCell[T](override val delegate: JFXTreeCellUpdateDelegate[T] = new JFXTreeCellUpdateDelegate[T])
+        extends TreeCell[T](delegate)
+        with SFXDelegate[JFXTreeCellUpdateDelegate[T]] {
 
     delegate.updateAction = Some({(item: T, empty: Boolean) => updateItem(item, empty)})
 
@@ -22,11 +25,11 @@ object CustomTreeCell {
         private[ui] var updateAction: Option[(T, Boolean) => _] = None
 
         final override def updateItem(item: T, empty: Boolean): Unit = {
-            super.updateItem(item, empty)
             updateAction match {
                 case Some(action) => action(item, empty)
                 case None => // do nothing
             }
+            super.updateItem(item, empty)
         }
     }
 }
