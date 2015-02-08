0fec7291495a86c501d96ababa70dbfb502fea8c
Roman Hargrave <roman@hargrave.info>
2015-02-07 20:30:57 -0600

    Changed the internal representation of PositionView to allow for modification of values.

--- src/main/scala/info/hargrave/composer/ui/PositionView.scala
+++ src/main/scala/info/hargrave/composer/ui/PositionView.scala
@@ -13,43 +13,75 @@ import scalafx.scene.layout.HBox
 import scalafx.util.converter.NumberStringConverter
 
 /**
- * Allows for the display (and editing) of a  [[Position]]
+ * Allows for the display of a  [[Position]]
+ * In an editing situation, it will not directly modify the position.
  */
-class PositionView(position: Position) extends HBox with Editable {
+class PositionView private() extends HBox with Editable {
 
-    private val lengthLabel = new Label(t"ui.common.noun_minutes")
-    private val separator = new Label(":")
+    val minutesProperty = new ObjectProperty[Number]
+    val secondsProperty = new ObjectProperty[Number]
+    val framesProperty  = new ObjectProperty[Number]
+
+    def this(minutes: Int, seconds: Int, frames: Int) = {
+        this()
+        this.minutes = minutes
+        this.seconds = seconds
+        this.frames  = frames
+    }
+
+    def this(position: Position) = {
+        this(position.minutes.getOrElse(0), position.seconds.getOrElse(0), position.frames.getOrElse(0))
+    }
+
+    private val minuteSeparator = new Label(":")
+    private val secondSeparator = new Label(":")
 
     val converterProperty = ObjectProperty(new NumberStringConverter)
 
     private val minuteSpinner = new NumberSpinner(0, 99) {
-        valueProperty.onChange {
-                                   position.minutes = Option(converter.fromString(getText).intValue)
-                               }
-        valueProperty.value = position.minutes.getOrElse[Int](0)
+        valueProperty.bind(minutesProperty)
     }
     private val secondSpinner = new NumberSpinner(0, 99) {
-        valueProperty.onChange {
-                                   position.seconds = Option(converter.fromString(getText).intValue)
-                               }
-        valueProperty.value = position.seconds.getOrElse[Int](0)
+        valueProperty.bind(secondsProperty)
     }
     private val frameSpinner = new NumberSpinner(0, 75) {
-        valueProperty.onChange {
-                                   position.frames = Option(converter.fromString(getText).intValue)
-                               }
-        valueProperty.value = position.seconds.getOrElse[Int](0)
+        valueProperty.bind(framesProperty)
     }
 
     minuteSpinner.editableProperty.bind(editableProperty)
     secondSpinner.editableProperty.bind(editableProperty)
     frameSpinner.editableProperty.bind(editableProperty)
+
     minuteSpinner.numberStringConverterProperty.bind(converterProperty)
     secondSpinner.numberStringConverterProperty.bind(converterProperty)
     frameSpinner.numberStringConverterProperty.bind(converterProperty)
 
+    minuteSpinner.prefWidthProperty.bind(prefWidth / 3)
+    secondSpinner.prefWidthProperty.bind(prefWidth / 3)
+    frameSpinner.prefWidthProperty.bind(prefWidth / 3)
+
+    prefWidth = 200
+
     final def converter = converterProperty.value
     final def converter_=(newValue: NumberStringConverter) = converterProperty.value = newValue
 
-    children = Seq[Node](minuteSpinner, secondSpinner, frameSpinner)
+    children = Seq[Node](minuteSpinner, minuteSeparator,
+                         secondSpinner, secondSeparator,
+                         frameSpinner)
+
+    final def minutes = converterProperty.value.fromString(Option(minuteSpinner.getText).getOrElse("0")).intValue
+    final def minutes_=(int: Int) = minutesProperty.value = int
+
+    final def seconds = converterProperty.value.fromString(Option(secondSpinner.getText).getOrElse("0")).intValue
+    final def seconds_=(int: Int) = secondsProperty.value = int
+
+    final def frames = converterProperty.value.fromString(Option(frameSpinner.getText).getOrElse("0")).intValue
+    final def frames_=(int: Int) = framesProperty.value = int
+
+    final def value = new Position(minutes, seconds, frames)
+    final def value_=(pos: Position): Unit = {
+        minutes = pos.minutes.getOrElse(0)
+        seconds = pos.seconds.getOrElse(0)
+        frames  = pos.frames.getOrElse(0)
+    }
 }
