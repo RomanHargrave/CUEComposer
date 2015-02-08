2ced77ee899e1295ee75e89c0a8bf4d837596cd8
Roman Hargrave <roman@hargrave.info>
2015-02-07 23:08:27 -0600

    Add properties editor to trackdataview

--- src/main/scala/info/hargrave/composer/ui/TrackDataView.scala
+++ src/main/scala/info/hargrave/composer/ui/TrackDataView.scala
@@ -1,17 +1,14 @@
 package info.hargrave.composer.ui
 
-import com.blogspot.myjavafx.NumberSpinner
-import jwbroek.cuelib.{Position, Index, TrackData}
+import jwbroek.cuelib.{Position, TrackData}
 
-import scalafx.beans.property.{ObjectProperty, StringProperty, BooleanProperty}
 import scalafx.collections.ObservableBuffer
-import scalafx.scene.control.TableColumn.CellDataFeatures
-import scalafx.scene.control.{SplitPane, TableCell, TableColumn, TableView}
-import scalafx.scene.layout.{HBox, GridPane, VBox, Priority}
+import scalafx.scene.control._
+import scalafx.scene.layout.{GridPane, VBox, Priority}
 import scalafx.Includes._
 
 import javafx.geometry.Insets
-import javafx.scene.control.{TableCell => JFXTableCell}
+import javafx.scene.control.{CheckMenuItem => JFXCheckMenuItem}
 
 import info.hargrave.composer._
 import info.hargrave.composer.util.CUEUtilities._
@@ -36,12 +33,81 @@ class TrackDataView(trackData: TrackData) extends SplitPane with Editable {
 
     items.add(metadataView)
 
-    // Index/Properties Editor -----------------------------------------------------------------------------------------
+    // Right Half ------------------------------------------------------------------------------------------------------
 
-    private val indexView = new IndexTableView(trackData.getIndices.asScala) {
-        editable = true
+    private val rightPane = new VBox {
+        fillWidth = true
     }
 
-    items.add(indexView)
+    items.add(rightPane)
+
+    // ----- Index Editor ----------------------------------------------------------------------------------------------
+
+    private val indexView = new IndexTableView(trackData.getIndices.asScala) {
+        editable    = true
+        vgrow       = Priority.Always
+    }
+
+    rightPane.children.add(indexView)
+
+    // ----- Properties Editor -----------------------------------------------------------------------------------------
+
+    private val dataTypeLabel   = new Label(t"ui.td_view.data_type")
+    private val dataTypeCombo   = new ChoiceBox[String] {
+        items = ObservableBuffer(TrackEntry.DataTypes)
+        value = trackData.dataType.orNull
+        value.onChange { trackData.setDataType(value.value) }
+
+        editableProperty.onChange { disable = !editable }
+    }
+
+    private val flagsLabel      = new Label(t"ui.td_view.flags")
+    private val flagsMenu       = new MenuButton {
+
+        def synchronizeItems(): Unit = {
+            val selectedItems   = items.filter { case(item: JFXCheckMenuItem) => item.selected.value }
+            val selectedStrings = selectedItems.map(_.text.value).sorted
+            setText(selectedStrings)
+            trackData.flags = selectedStrings
+        }
+
+        def setText(flags: Iterable[String]): Unit = text = flags.mkString(", ")
+
+        items = TrackEntry.Flags.map {flag => new CheckMenuItem(flag){
+            onAction = () => synchronizeItems()
+            selected = trackData.flags.exists(_ == flag)
+        }}
+
+        synchronizeItems()
+    }
+
+    private val pregapPosition  = new PositionView(trackData.pregap.getOrElse(new Position))
+    private val pregapCheck     = new CheckBox(t"ui.td_view.pregap") {
+        selected = trackData.pregap.isDefined
+        selected.onChange { trackData.pregap = if(selected.value) Option(pregapPosition.value) else None }
+        pregapPosition.editableProperty.bind(selected)
+    }
+
+    private val postgapPosition = new PositionView(trackData.postgap.getOrElse(new Position))
+    private val postgapCheck    = new CheckBox(t"ui.td_view.postgap") {
+        selected = trackData.postgap.isDefined
+        selected.onChange { trackData.postgap = if(selected.value) Option(postgapPosition.value) else None }
+        postgapPosition.editableProperty.bind(selected)
+    }
+
+    private val propertiesPane  = new GridPane {
+        vgap = 5
+        hgap = 10
+        padding = new Insets(10, 10, 10, 10)
+
+        prefHeight.bind(minHeight)
+
+        addRow(0, dataTypeLabel, dataTypeCombo)
+        addRow(1, flagsLabel, flagsMenu)
+        addRow(2, pregapCheck, pregapPosition)
+        addRow(3, postgapCheck, postgapPosition)
+    }
+
+    rightPane.children.add(propertiesPane)
 
 }
