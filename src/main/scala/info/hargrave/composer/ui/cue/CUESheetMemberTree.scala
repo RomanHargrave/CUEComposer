fe0ddaa1be5898f1c7765e59c6f1b69b6bda1843
Roman Hargrave <roman@hargrave.info>
2015-02-21 15:33:06 -0600

    Set up the CUESheet member tree to use reactive properties when naming.
    At present, JavaFX does not update the cell skin when the text property is changed (wtf?) so a relayout of the treeview must happen in order for the appearance of the FileData/TrackData representation to change.

--- src/main/scala/info/hargrave/composer/ui/cue/CUESheetMemberTree.scala
+++ src/main/scala/info/hargrave/composer/ui/cue/CUESheetMemberTree.scala
@@ -2,6 +2,7 @@ package info.hargrave.composer.ui.cue
 
 import info.hargrave.composer._
 import info.hargrave.composer.ui.cue.CUESheetMemberTree.CueEntryCell
+import info.hargrave.composer.ui.cue.cuelib._
 import info.hargrave.composer.ui.{CustomTreeCell, Editable}
 import info.hargrave.composer.util.CUEUtilities._
 import jwbroek.cuelib.{CueSheet, FileData, TrackData}
@@ -15,7 +16,8 @@ import scalafx.scene.control._
 import scalafx.scene.layout.{Priority, VBox}
 
 /**
- * Displays a CUE Sheet as a two-level TreeView[Either[FileData,TrackData]] from which the user can select file sections,
+ * Displays a CUE Sheet as a two-level TreeView[Either[FileData,TrackData]] from which the user can select file
+ * sections,
  * or track declarations within the sections.
  *
  * It provides a selection callback that will provide the Either[...] selected by the user.
@@ -24,15 +26,16 @@ import scalafx.scene.layout.{Priority, VBox}
  */
 class CUESheetMemberTree(sheet: CueSheet) extends VBox with Editable {
 
-    val filesProperty   = ObservableBuffer(Seq[FileData]())
+    import info.hargrave.composer.ui.cue.CUESheetMemberTree.ObservableMember
+
+    val filesProperty = ObservableBuffer(Seq[FileData]())
 
     // Element List ----------------------------------------------------------------------------------------------------
-
-    private val elementsList = new TreeView[Either[FileData, TrackData]] {
-        root        = new TreeItem
-        showRoot    = false
+    private val elementsList = new TreeView[ObservableMember] {
+        root = new TreeItem
+        showRoot = false
         cellFactory = view => new CueEntryCell
-        vgrow       = Priority.Always
+        vgrow = Priority.Always
     }
 
     private def synchronizeData(): Unit = {
@@ -41,24 +44,28 @@ class CUESheetMemberTree(sheet: CueSheet) extends VBox with Editable {
          * For each file entry create an item, then construct a sequence of items for each track entry and assign them
          * to the file item as children.
          */
-        elementsList.root.value.children = filesProperty.map(data =>
-            new TreeItem[Either[FileData, TrackData]](Left(data)) {
-                children = data.trackData.map(tData => new TreeItem[Either[FileData, TrackData]](Right(tData)))
-            })
+        elementsList.root.value.children = filesProperty.map(data => {
+            new TreeItem[ObservableMember](Left(data: ObservableFileData)) {
+                children = data.trackData.map(tData => {
+                    new TreeItem[ObservableMember](Right(tData: ObservableTrackData))
+                })
+            }
+        })
     }
 
-    filesProperty.onChange { synchronizeData() }
+    filesProperty.onChange {
+                               synchronizeData()
+                           }
 
     // Toolbar ---------------------------------------------------------------------------------------------------------
-
     private val elementsToolbar = new ToolBar {
         visible.bind(editableProperty)
     }
-    private val addMemberBtn    = new MenuButton {
+    private val addMemberBtn = new MenuButton {
 
-        text    = t"ui.common.verb_add"
+        text = t"ui.common.verb_add"
 
-        val fileMemberOption    = new MenuItem {
+        val fileMemberOption = new MenuItem {
 
             text = t"cuesheet.file_entry"
 
@@ -66,46 +73,57 @@ class CUESheetMemberTree(sheet: CueSheet) extends VBox with Editable {
                 disable = filesProperty.length >= 99
             }
 
-            filesProperty.onChange { updateDisabled() }
+            filesProperty.onChange {
+                                       updateDisabled()
+                                   }
             onAction = () => filesProperty += new FileData(sheet)
         }
 
-        val trackMemberOption   = new MenuItem {
+        val trackMemberOption = new MenuItem {
 
             text = t"cuesheet.track_entry"
 
-            def updateDisabled(): Unit = disable = selectedItem match {
-                case Some(Left(fileData)) => fileData.trackData.length >= 99
-                case _ => false
+            def updateDisabled(): Unit = {
+                disable = selectedItem match {
+                    case Some(Left(fileData)) => fileData.trackData.length >= 99
+                    case _                    => false
+                }
             }
 
             onAction = () => selectedItem.get.left.get.getTrackData.add(new TrackData(selectedItem.get.left.get))
 
-            filesProperty.onChange { updateDisabled() }
-            onSelectionChanged {(ign: Option[Either[FileData, TrackData]]) => updateDisabled() }
+            filesProperty.onChange {
+                                       updateDisabled()
+                                   }
+            onSelectionChanged { (ign: Option[Either[FileData, TrackData]]) => updateDisabled()}
         }
 
-        items   = Seq(fileMemberOption, trackMemberOption)
+        items = Seq(fileMemberOption, trackMemberOption)
     }
-    private val delMemberBtn    = new Button {
+    private val delMemberBtn = new Button {
 
         def updateDisabled(): Unit = {
             disable = selectedItem.isEmpty || filesProperty.isEmpty
         }
 
-        filesProperty.onChange { updateDisabled() }
-        onSelectionChanged {(ign: Option[Either[FileData, TrackData]]) => updateDisabled() }
+        filesProperty.onChange {
+                                   updateDisabled()
+                               }
+        onSelectionChanged { (ign: Option[Either[FileData, TrackData]]) => updateDisabled()}
 
-        onAction = () => selectedItem match {
-            case Some(Left(fileData))   =>
-                filesProperty.remove(fileData)
-            case Some(Right(trackData)) =>
+        onAction = () => {
+            selectedItem match {
+                case Some(Left(fileData))   =>
+                    filesProperty.remove(fileData)
+                case Some(Right(trackData)) =>
 
-                trackData.parent.getTrackData.remove(trackData)
+                    trackData.parent.getTrackData.remove(trackData)
 
-                elementsList.selectionModel.value.select(null)
+                    elementsList.selectionModel.value.select(null)
 
-                synchronizeData()
+                    synchronizeData()
+                case None                   => // Nothing
+            }
         }
 
         text = t"ui.common.verb_remove"
@@ -120,32 +138,60 @@ class CUESheetMemberTree(sheet: CueSheet) extends VBox with Editable {
     filesProperty.addAll(sheet.fileData)
 
     // Component API ---------------------------------------------------------------------------------------------------
-
-    final def selectedItem = elementsList.selectionModel.value.getSelectedItem match {
-        case null       => None
-        case propItem   => Option(propItem.value.value)
-    }
-
-    final def onSelectionChanged(op: Option[Either[FileData, TrackData]]=>_): Subscription =
-        elementsList.selectionModel.value.selectedItemProperty.onChange ({
-            op(selectedItem)
-            ()
-        })
-}
-object CUESheetMemberTree {
-    final class CueEntryCell extends CustomTreeCell[Either[FileData, TrackData]] {
-
-        override def updateItem(item: Either[FileData, TrackData], empty: Boolean): Unit = empty match {
-            case true =>
-                text = null
-            case false =>
-                text = item match {
-                    case null => null // oh java
-                    case Left(fileData)     =>
-                        s"${fileData.getFileType} ${fileData.getFile}"
-                    case Right(trackData)   =>
-                        if(trackData.getNumber > 0) tf"ui.cue.track_entry"(trackData.getNumber) else t"ui.cue.undefined_track"
-                }
+    final def selectedItem = {
+        elementsList.selectionModel.value.getSelectedItem match {
+            case null     => None
+            case propItem => Option(propItem.value.value)
         }
     }
+
+    final def onSelectionChanged(op: Option[Either[FileData, TrackData]] => _): Subscription = {
+        elementsList.selectionModel.value.selectedItemProperty.onChange({
+                                                                            op(selectedItem)
+                                                                            ()
+                                                                        })
+    }
+}
+
+object CUESheetMemberTree {
+
+    import info.hargrave.composer.ui.cue.cuelib.{Observability, ObservableFileData, ObservableTrackData}
+
+    type ObservableMember = Either[ObservableFileData, ObservableTrackData]
+
+    final class CueEntryCell extends CustomTreeCell[ObservableMember] {
+
+        private var childSubscription: Option[Subscription] = None
+
+        override def updateItem(item: ObservableMember, empty: Boolean): Unit = {
+            empty match {
+                case true  =>
+                    if (childSubscription.isDefined) {
+                        childSubscription.get.cancel()
+                        childSubscription = None
+                    }
+                    text = null
+                case false =>
+                    item match {
+                        case Left(fileData)   =>
+                            childSubscription =
+                                    Option(fileData.onInvalidate { text = s"${fileData.getFileType} ${fileData.getFile}" })
+
+                            fileData.invalidate()
+                        case Right(trackData) =>
+                            childSubscription =
+                                    Option(trackData.onInvalidate {   text =  if (trackData.getNumber > 0) {
+                                                                                  tf"ui.cue.track_entry"(trackData.getNumber)
+                                                                              } else {
+                                                                                  t"ui.cue.undefined_track"
+                                                                              }
+                                                                  })
+
+
+                            trackData.invalidate()
+                    }
+            }
+        }
+    }
+
 }
