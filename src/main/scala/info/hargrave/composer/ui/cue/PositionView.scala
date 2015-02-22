cbf583c7884912e3a48d79ee257875a2c0ccf233
Roman Hargrave <roman@hargrave.info>
2015-02-22 01:29:48 -0600

    update PositionView to talk observability

--- src/main/scala/info/hargrave/composer/ui/cue/PositionView.scala
+++ src/main/scala/info/hargrave/composer/ui/cue/PositionView.scala
@@ -2,11 +2,12 @@ package info.hargrave.composer.ui.cue
 
 import com.blogspot.myjavafx.NumberSpinner
 import info.hargrave.composer.ui.Editable
+import info.hargrave.composer.ui.cue.cuelib.{Observability, ObservablePosition}
 import info.hargrave.composer.util.CUEUtilities._
 import jwbroek.cuelib.Position
 
 import scalafx.Includes._
-import scalafx.beans.property.ObjectProperty
+import scalafx.beans.property.{IntegerProperty, ObjectProperty}
 import scalafx.scene.Node
 import scalafx.scene.control.Label
 import scalafx.scene.layout.HBox
@@ -16,21 +17,28 @@ import scalafx.util.converter.NumberStringConverter
  * Allows for the display of a  [[Position]]
  * In an editing situation, it will not directly modify the position.
  */
-class PositionView private() extends HBox with Editable {
+class PositionView private() extends HBox with Editable with Observability {
 
-    val minutesProperty = new ObjectProperty[Number]
-    val secondsProperty = new ObjectProperty[Number]
-    val framesProperty  = new ObjectProperty[Number]
+
+    val minutesProperty = new IntegerProperty
+    minutesProperty.invalidatesParent()
+    val secondsProperty = new IntegerProperty
+    secondsProperty.invalidatesParent()
+    val framesProperty = new IntegerProperty
+    framesProperty.invalidatesParent()
 
     def this(minutes: Int, seconds: Int, frames: Int) = {
         this()
         this.minutes = minutes
         this.seconds = seconds
-        this.frames  = frames
+        this.frames = frames
     }
 
-    def this(position: Position) = {
-        this(position.minutes.getOrElse(0), position.seconds.getOrElse(0), position.frames.getOrElse(0))
+    def this(position: ObservablePosition) = {
+        this()
+        minutesProperty.bindBidirectional(position.minutesProperty)
+        secondsProperty.bindBidirectional(position.secondsProperty)
+        framesProperty.bindBidirectional(position.framesProperty)
     }
 
     private val minuteSeparator = new Label(":")
@@ -46,7 +54,7 @@ class PositionView private() extends HBox with Editable {
         valueProperty().bindBidirectional(secondsProperty)
         editableProperty.onChange { disable = !isEditable }
     }
-    private val frameSpinner = new NumberSpinner(0, 75) {
+    private val frameSpinner  = new NumberSpinner(0, 75) {
         valueProperty().bindBidirectional(framesProperty)
         editableProperty.onChange { disable = !isEditable }
     }
@@ -66,25 +74,28 @@ class PositionView private() extends HBox with Editable {
     minWidth = 200
 
     final def converter = converterProperty.value
+
     final def converter_=(newValue: NumberStringConverter) = converterProperty.value = newValue
 
-    children = Seq[Node](minuteSpinner, minuteSeparator,
-                         secondSpinner, secondSeparator,
-                         frameSpinner)
+    children = Seq[Node](minuteSpinner, minuteSeparator, secondSpinner, secondSeparator, frameSpinner)
 
-    final def minutes = minutesProperty.value.intValue
+    final def minutes = minutesProperty.value
+
     final def minutes_=(int: Int) = minutesProperty.value = int
 
-    final def seconds = secondsProperty.value.intValue
+    final def seconds = secondsProperty.value
+
     final def seconds_=(int: Int) = secondsProperty.value = int
 
-    final def frames = framesProperty.value.intValue
+    final def frames = framesProperty.value
+
     final def frames_=(int: Int) = framesProperty.value = int
 
     final def value = new Position(minutes, seconds, frames)
+
     final def value_=(pos: Position): Unit = {
         minutes = pos.minutes.getOrElse(0)
         seconds = pos.seconds.getOrElse(0)
-        frames  = pos.frames.getOrElse(0)
+        frames = pos.frames.getOrElse(0)
     }
 }
