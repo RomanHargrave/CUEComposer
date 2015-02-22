e72ed3f4e826f467cd5198c101cc2d4c1fe9d35f
Roman Hargrave <roman@hargrave.info>
2015-02-22 01:56:06 -0600

    Add node decorator to Editable

--- src/main/scala/info/hargrave/composer/ui/Editable.scala
+++ src/main/scala/info/hargrave/composer/ui/Editable.scala
@@ -7,8 +7,26 @@ import scalafx.beans.property.BooleanProperty
  */
 trait Editable {
 
+    import javafx.scene.Node
+
     val editableProperty = new BooleanProperty()
 
     def editable = editableProperty.value
     def editable_=(bool: Boolean) = editableProperty.value = bool
+
+    implicit class OtherEditableDecorations(other: Editable) {
+
+        def bindEditable() = other.editableProperty.bind(editableProperty)
+    }
+
+    /**
+     * Allows for calling dependsOnEditability() on a node to make it depend on the editability of the enclosing
+     * Editable
+     *
+     * @param node implicit node
+     */
+    implicit class NodeDecorations(node: Node) {
+
+        def bindEditable() = node.disableProperty.bind(!editableProperty)
+    }
 }
