27f5bec35412bc7bfeca16bad8318e864832c140
Roman Hargrave <roman@hargrave.info>
2015-02-23 15:32:23 -0600

    Shorten CustomTreeCell massively by not proxying everything to a custom class. Just create an anonymous class and use it as a delegate to forward updateItem to the CustomTreeCell

--- src/main/scala/info/hargrave/composer/ui/CustomTreeCell.scala
+++ src/main/scala/info/hargrave/composer/ui/CustomTreeCell.scala
@@ -2,34 +2,20 @@ package info.hargrave.composer.ui
 
 import javafx.scene.control.{TreeCell => JFXTreeCell}
 
-import info.hargrave.composer.ui.CustomTreeCell.JFXTreeCellUpdateDelegate
-
-import scalafx.delegate.SFXDelegate
 import scalafx.scene.control.TreeCell
 
 /**
  * Provides a CustomTreeCell that allows for implementation of the UpdateItem method
  */
-abstract class CustomTreeCell[T](override val delegate: JFXTreeCellUpdateDelegate[T] = new JFXTreeCellUpdateDelegate[T])
-        extends TreeCell[T](delegate)
-        with SFXDelegate[JFXTreeCellUpdateDelegate[T]] {
+abstract class CustomTreeCell[T] extends TreeCell[T] {
 
-    delegate.updateAction = Some({(item: T, empty: Boolean) => updateItem(item, empty)})
+    self =>
+
+    override val delegate = new JFXTreeCell[T] {
+        override def updateItem(item: T, empty: Boolean): Unit = self.updateItem(item, empty)
+
+        override def toString: String = s"[CustomTreeCell Anonymous Delegate] ${super.toString}"
+    }
 
     def updateItem(item: T, empty: Boolean): Unit
-}
-object CustomTreeCell {
-
-    class JFXTreeCellUpdateDelegate[T] extends JFXTreeCell[T] {
-
-        private[ui] var updateAction: Option[(T, Boolean) => _] = None
-
-        final override def updateItem(item: T, empty: Boolean): Unit = {
-            updateAction match {
-                case Some(action) => action(item, empty)
-                case None => // do nothing
-            }
-            super.updateItem(item, empty)
-        }
-    }
 }
